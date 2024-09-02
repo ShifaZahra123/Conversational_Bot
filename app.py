65a4e63d872cc6ad62ea050780b18584c515aead
Shifa Zahra <140159309+ShifaZahra123@users.noreply.github.com>
2024-09-02 19:52:06 +0500

    app.py

--- app.py
+++ app.py
@@ -1,27 +1,25 @@
-import streamlit as st
 from dotenv import load_dotenv
+load_dotenv() ## loading all the environment variables
 
-load_dotenv() ##load all the nevironment variables
+import streamlit as st
 import os
 import google.generativeai as genai
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-model = genai.GenerativeModel("gemini-pro")
-chat = model.start_chat(history=[])
-
 ## function to load Gemini Pro model and get repsonses
-
+model=genai.GenerativeModel("gemini-pro") 
+chat = model.start_chat(history=[])
 def get_gemini_response(question):
     
-    response=chat.send_message(question,stream=True) # Stream it and then Display, get access of all streaming data
+    response=chat.send_message(question,stream=True)  # Stream it and then Display, get access of all streaming data
     return response
 
 ##initialize our streamlit app
 
-st.set_page_config(page_title="Q&A Chatbot")
+st.set_page_config(page_title="Q&A Bot")
 
-st.header("AI Chatbot")
+st.header("Conversational Chatbot")
 
 # Initialize session state for chat history if it doesn't exist
 if 'chat_history' not in st.session_state:
@@ -38,9 +36,8 @@ if submit and input:
     for chunk in response:
         st.write(chunk.text)
         st.session_state['chat_history'].append(("Bot", chunk.text))
-    # Clear the input field after submission
+     # Clear the input field after submission
     st.session_state.input = ""  # Reset the text input field
-    
 st.subheader("The Chat History is")
     
 for role, text in st.session_state['chat_history']:
